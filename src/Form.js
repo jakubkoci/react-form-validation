649ba58f4543e6a11bf14e0a8c9f66ab1ea9b25e
jakubkoci <jakub.koci@gmail.com>
2018-03-18 23:12:07 +0100

    Add simple validation

--- src/Form.js
+++ src/Form.js
@@ -16,20 +16,26 @@ export default function Form() {
       validate={validate}
       onSubmit={onSubmit}
       render={props => {
-        const { handleChange, handleSubmit } = props
+        const { isSubmitting, errors, handleChange, handleSubmit } = props
 
         return (
           <div className="form">
             <label className="form-field" htmlFor="email">
               E-mail: <input name="email" type="email" onChange={handleChange} />
             </label>
+            <div className="form-field-error">{errors.email}</div>
+
             <label className="form-field" htmlFor="password">
               Password: <input name="password" type="password" onChange={handleChange} />
             </label>
+            <div className="form-field-error">{errors.password}</div>
+
             <label className="form-field" htmlFor="passwordConfirmation">
               Confirm password: <input name="passwordConfirmation" type="password" onChange={handleChange} />
             </label>
-            <button onClick={handleSubmit}>Sign Up</button>
+            <div className="form-field-error">{errors.passwordConfirmation}</div>
+
+            <button onClick={handleSubmit}>{isSubmitting ? 'Loading' : 'Sign Up'}</button>
           </div>
         )
       }}
@@ -38,7 +44,25 @@ export default function Form() {
 }
 
 function validate(values) {
-  return {}
+  const errors = {}
+
+  if (!values.email) {
+    errors.email = 'E-mail is required!'
+  }
+
+  if (!values.password) {
+    errors.password = 'Password is required!'
+  } else if (values.password.length < 6) {
+    errors.password = 'Password has to be longer than 6 characters'
+  }
+
+  if (!values.passwordConfirmation) {
+    errors.passwordConfirmation = 'Password confirmation is required!'
+  } else if (values.password !== values.passwordConfirmation) {
+    errors.passwordConfirmation = 'Passwords are not the same!'
+  }
+
+  return errors
 }
 
 function onSubmit(values, { setSubmitting, setErrors }) {
