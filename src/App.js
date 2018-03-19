d106826ff8bfff6e5777b806a950edd938ec20bc
jakubkoci <jakub.koci@gmail.com>
2018-03-19 22:28:32 +0100

    Refactor further to standalone functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Form from './Form'
+import SignUpForm from './components/SignUpForm'
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,7 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Form />
+        <SignUpForm />
       </div>
     );
   }
